c63c253294692fe5f705929b5aa84b691551f0b1
remnantsofred <remnantsofred@gmail.com>
2022-10-10 15:34:21 -0700

    finished bootcamp project

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,75 @@
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new { |h, k| h[k] = [] } 
+    end
+
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        @teachers << teacher
+    end
+
+    def enroll(student)
+        if @students.length < @student_capacity
+            @students << student
+            return true
+        else
+            return false
+        end
+    end
+        
+    def enrolled?(student)
+        @students.include?(student)
+    end
+
+    def student_to_teacher_ratio
+        @students.length / @teachers.length
+    end
+
+    def add_grade(student, grade)
+        if students.include?(student)
+            @grades[student] << grade
+            return true
+        else
+            return false
+        end
+    end
+
+    def num_grades(student)
+        if @students.include?(student)
+            @grades[student].length
+        end
+    end
+
+    def average_grade(student)
+        if @students.include?(student) && @grades[student].length > 0
+            sum = 0
+            @grades[student].each do |grade|
+                sum += grade
+            end
+            return (sum / @grades[student].length)
+        elsif !@students.include?(student) || @grades[student].length == 0
+            return nil
+        end
+    end
+
 end
