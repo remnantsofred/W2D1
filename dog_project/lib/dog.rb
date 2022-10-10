4e29264310793caca6fbeedfe86f350552f2f3a4
remnantsofred <remnantsofred@gmail.com>
2022-10-10 13:42:38 -0700

    adding project folders

---
+++ dog_project/lib/dog.rb
@@ -0,0 +1,37 @@
+class Dog
+    def initialize
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+    
+    def age
+        @age
+    end
+    
+    def bark
+        @bark
+    end
+    
+    def name
+        @name
+    end
+    
+    def favorite_foods
+        @favorite_foods
+    end
+    
+
+
+end
+ 
